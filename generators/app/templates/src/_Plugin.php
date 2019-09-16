420fc6a0b74d0575623da74776a9105f04f6cd33
Daniel Kudwien <daniel@netzstrategen.com>
2019-09-16 15:58:38 +0200

    fixup plugin base path/url

--- generators/app/templates/src/_Plugin.php
+++ generators/app/templates/src/_Plugin.php
@@ -27,6 +27,11 @@ class Plugin {
   const L10N = self::PREFIX;
 
   /**
+   * @var string
+   */
+  private static $baseUrl;
+
+  /**
    * Plugin initialization method.
    *
    * @implements init
@@ -42,6 +47,27 @@ class Plugin {
    */
   public static function loadTextdomain() {
     load_plugin_textdomain(static::L10N, FALSE, static::L10N . '/languages/');
+  }
+
+  /**
+   * The absolute filesystem base path of this plugin.
+   *
+   * @return string
+   */
+  public static function getBasePath() {
+    return dirname(__DIR__);
+  }
+
+  /**
+   * The base URL path to this plugin's folder.
+   *
+   * Uses plugins_url() instead of plugin_dir_url() to avoid a trailing slash.
+   */
+  public static function getBaseUrl() {
+    if (!isset(static::$baseUrl)) {
+      static::$baseUrl = plugins_url('', static::getBasePath() . '/plugin.php');
+    }
+    return static::$baseUrl;
   }
 
 }
